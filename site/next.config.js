66eebcdda6bf1b1422159303ea84c6e98289d811
chriswilson2020 <73828727+chriswilson2020@users.noreply.github.com>
2024-07-12 15:42:42 +0200

    Update next.config.js

--- site/next.config.js
+++ site/next.config.js
@@ -2,15 +2,15 @@
 
 const nextConfig = {
   env: {
-    name: 'Kasm Technologies',
-    description: 'The official store for Kasm supported workspaces.',
-    icon: '/img/logo.svg',
-    listUrl: 'https://registry.kasmweb.com/',
-    contactUrl: 'https://kasmweb.com/support',
+    name: 'Unofficial nchat',
+    description: 'My unofficial nchat build',
+    icon: 'https://user-images.githubusercontent.com/5698566/230345149-ef757e51-6eb9-479d-94f5-a13e4ad33b03.png',
+    listUrl: 'https://chriswilson2020.github.com/kasm-workspaces-nchat/',
+    contactUrl: 'https://chriswilson2020.github.com/kasm-workspaces-nchat/issues/',
   },
   reactStrictMode: true,
   swcMinify: true,
-  basePath: '/kasm-registry/1.0',
+  basePath: '/kasm-workspaces-nchat/1.0',
   trailingSlash: true,
   images: {
     unoptimized: true,
